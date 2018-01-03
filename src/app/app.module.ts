ee5da6f3d2956211cc743bf5f95eb4c360329193
Always Hungry <suico.dave@gmail.com>
2018-01-03 13:59:24 +0800

    Added routing, resolving department pre-fetch on IndexComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MyMaterialModule } from './my-material-module';
+import { IndexComponent } from './index/index.component';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './guards/auth.guard';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DepartmentService } from './services/department.service';
+import { DepartmentResolver } from './resolves/department.resolve';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    AuthComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MyMaterialModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard, DepartmentService, DepartmentResolver],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
