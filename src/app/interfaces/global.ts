820921f098f9555b92fd4685270edbd832377fa8
Always Hungry <suico.dave@gmail.com>
2018-02-11 00:47:53 +0800

    fixed merge conflict

--- src/app/interfaces/global.ts
+++ src/app/interfaces/global.ts
@@ -1,11 +1,7 @@
 import { HttpHeaders } from '@angular/common/http';
 
 
-<<<<<<< HEAD
 export const apiUrl = 'https://sces-api.herokuapp.com/api/';
-=======
-export const apiUrl = 'http://localhost:8000/api/';
->>>>>>> master
 
 export const apiHeaders = new HttpHeaders({
     'X-Requested-With': 'XMLHttpRequest',
